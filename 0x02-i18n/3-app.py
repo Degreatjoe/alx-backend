6faf4c26397bb1e72195963537de1f97aea0696d
Great <jgreat7702gmail.com>
2024-11-05 15:32:14 +0100

    task3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+the flask application
+"""
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext as _
 
@@ -7,6 +10,9 @@ app = Flask(__name__)
 
 
 class Config:
+    """
+    the config class
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,11 +26,17 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
+    """
+    the get locale function
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
+    """
+    the index function starting point
+    """
     return render_template('0-index.html')
 
 
