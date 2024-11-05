0aa80f663f420c7bf92a63612c627d899bbe3c04
Great <jgreat7702gmail.com>
2024-11-05 14:35:43 +0100

    task1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+# Step 3: Define Config Class
+class Config:
+    LANGUAGES = ["en", "fr"]  # Available languages
+    BABEL_DEFAULT_LOCALE = "en"  # Default locale
+    BABEL_DEFAULT_TIMEZONE = "UTC"  # Default timezone
+
+
+app.config.from_object(Config)  # Use Config class for app configuration
+
+# Step 4: Instantiate Babel
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
