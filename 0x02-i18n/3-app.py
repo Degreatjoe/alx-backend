13daf28480ce7bcb4b03880d36b3fa546afda94a
Great <jgreat7702gmail.com>
2024-11-05 15:50:11 +0100

    task3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -37,7 +37,7 @@ def index():
     """
     the index function starting point
     """
-    return render_template('3-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
